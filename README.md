c9bc8739e888aa9e2cdc1e2d51b5f949580aad38
samyak <samyakseokar@gmail.com>
2023-05-24 11:06:36 +0530

    linked database

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Built REST API using Java spring boot Application without database
+Built REST API using Java spring boot Application with database
--- src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
+++ src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -3,67 +3,85 @@ package com.springrest.springrest.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
+
+import com.springrest.springrest.dao.CourseDao;
 import com.springrest.springrest.entities.Course;
 
 @Service
 public class CourseServiceImpl implements CourseService{
 	
-	List<Course> list;
+//	this course list is for temporary data storage without any database only for practice purpose, for that reason i commented out.
+//	List<Course> list;
+	
+	
+//	Here we are using courseDao database 
+	@Autowired
+	private CourseDao courseDao;
 	
 	public CourseServiceImpl() {
-		list=new ArrayList<>();
-		list.add(new Course(145, "java core Course", "this course basics of java"));
-		list.add(new Course(4343, "springs boot course", "creating rest api using spring boot"));
+//		list=new ArrayList<>();
+//		list.add(new Course(145, "java core Course", "this course basics of java"));
+//		list.add(new Course(4343, "springs boot course", "creating rest api using spring boot"));
 	}
 	
 
    @Override
-   public List<Course> getCourses(){
-   	   return list;
+   public List<Course> getCourses(){ 
+	   return courseDao.findAll();   
+//   	   return list;
    }
 
 
    @Override
    public Course getCourse(long courseId) {
 	   
-	   Course c = null;
-	   for(Course course:list) {
-		   if(course.getId() == courseId) {
-			   c = course;  
-		   }
-	   } 
-	   return c;
+//	   Course c = null;
+//	   for(Course course:list) {
+//		   if(course.getId() == courseId) {
+//			   c = course;  
+//		   }
+//	   } 
+//	   return c;
+	   
+	   return courseDao.getOne(courseId);
     }
 
 
 	@Override
 	public Course addCourse1(Course course) {
-		list.add(course);
+//		list.add(course);
+		
+		courseDao.save(course);
 		return course;
 	}
 
 
 	@Override
 	public Course updateCourse1(Course course) {
-		list.forEach(e -> {
-			if (e.getId() == course.getId()) {
-				e.setTitle(course.getTitle());
-				e.setDescription(course.getDescription());
-			}
-		});
+//		list.forEach(e -> {
+//			if (e.getId() == course.getId()) {
+//				e.setTitle(course.getTitle());
+//				e.setDescription(course.getDescription());
+//			}
+//		});
+		courseDao.save(course);
 		return course;
 	}
 
 
 	@Override
 	public void deleteCourse1(long courseId) {
-		for(Course course:list) {
-			if(course.getId()== courseId) {
-				list.remove(course);
-			}
-		}
+//		for(Course course:list) {
+//			if(course.getId()== courseId) {
+//				list.remove(course);
+//			}
+//		}
+		
+		Course entity = courseDao.getOne(courseId); 
+		courseDao.delete(entity);
 	}
 
    
